2b28ca83b7e0ea45a792c8793ce6458054087aeb
arifin2018 <arifingdr@gmail.com>
2023-01-15 14:37:31 +0700

    ternary operator

--- src/TernaryOperator.java
+++ src/TernaryOperator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TernaryOperator {
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int nilai = 75;
+
+        String ucapan = nilai >= 75 ? "hai" : "tidak hai";
+
+        System.out.println(ucapan);
+    }
 }
